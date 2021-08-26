aa4cbe55b1e7705c05ba41ca999e52be4d318921
KamilJavaProgrammer <Witchcraft15@interia.pl>
2021-08-26 13:28:50 +0200

    initial commit

---
+++ src/main/java/com/test/demo/Controllers/TestController.java
@@ -0,0 +1,24 @@
+package com.test.demo.Controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> getData(){
+        return  ResponseEntity.status(200).body("GithubTest");
+    }
+
+    @GetMapping("/test1")
+    public ResponseEntity<String> getData1(){
+        for(int i=0; i<100; i++)
+        {
+            System.out.println(i);
+        }
+        return  ResponseEntity.status(200).body("GithubTest1");
+    }
+}
